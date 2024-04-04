a2ef67477f849743312923201627e362e6270560
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2024-04-04 12:37:47 +0200

    maj

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "index", to: "pages#index"
   get "recherche", to: "pages#recherche"
+  get "a_propos", to: "pages#apropos"
   resources :categories
   resources :recipes
   get '/random', to: 'recipes#random'
