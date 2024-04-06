8003bd23998fbbe474d8244a7119b409aba248d6
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2024-04-06 18:19:38 +0200

    breadxrump

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,5 +7,10 @@ class CategoriesController < ApplicationController
     id, _ = params[:id].split('-')
     @category = Category.find(id)
     @recipes = @category.recipes.order(:name) if @category
+    if @category.present?
+      @breadcrumbs = [{ title: 'Home', url: root_path }, { title: 'Catégorie', url: categories_path }, { title: @category.name, url: category_path(@category) }]
+    else
+      @breadcrumbs = [{ title: 'Home', url: root_path }]
+    end
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,12 @@ class RecipesController < ApplicationController
     category_id = @recipe.category_id
     @next_recipe = Recipe.where("id > ? AND category_id = ?", @recipe.id, category_id).order(:id).first
     @previous_recipe = Recipe.where("id < ? AND category_id = ?", @recipe.id, category_id).order(id: :desc).first
+     @category = @recipe.category
+    if @category.present?
+      @breadcrumbs = [{ title: 'Home', url: root_path }, { title: 'Catégorie', url: categories_path }, { title: @category.name, url: category_path(@category) }, { title: @recipe.name, url: recipe_path(@recipe) }]
+    else
+      @breadcrumbs = [{ title: 'Home', url: root_path }, { title: @recipe.name, url: recipe_path(@recipe) }]
+    end
   end
 
   def random
