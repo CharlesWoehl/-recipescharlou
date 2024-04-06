69dad47322cd70abbd50873a2ce15aa70672cd8c
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2024-04-06 16:55:17 +0200

    mja

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   get "index", to: "pages#index"
   get "recherche", to: "pages#recherche"
@@ -6,12 +7,6 @@ Rails.application.routes.draw do
   resources :categories
   resources :recipes
   get '/random', to: 'recipes#random'
-
-  # Redirection pour la création de compte
-  constraints ->(request) { request.path.exclude?('users') } do
-    root to: "pages#home" # Redirection vers la page d'accueil pour les requêtes de création de compte
-  end
-
 
   get "up" => "rails/health#show", as: :rails_health_check
 
