466af55949f1b57eb6e4c5fbaf7af2d10ac32c3b
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2024-05-10 16:55:39 +0200

    maj index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def index
-    @recipes_by_letter = Recipe.all.group_by { |recipe| recipe.name[0].upcase }
-                             .sort_by { |letter, _recipes| letter }
+    if params[:section] == 'liste'
+      @recipes = Recipe.all.order("name ASC")
+    else
+      @recipes_by_letter = Recipe.all.group_by { |recipe| recipe.name[0].upcase }
+      .sort_by { |letter, _recipes| letter }
+    end
   end
+
 
   def recherche
     # Vérifiez si le paramètre de requête `query` est présent dans l'URL
